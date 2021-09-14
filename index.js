68cce5f7ab931a882eff10ea08681817c01e08a5
Nancy-codergirl <nancymuriithi@student.moringaschool.com>
2021-09-14 14:34:07 +0300

    Make contact form responsive

--- index.js
+++ index.js
@@ -1,3 +1,117 @@
 function style(){
     $("#what-we-do h5").addClass("text-center fw-bold")
     $("#what-we-do p").addClass("text-center")
+    $(document).ready(function(){
+ 
+
+        // toggle images to get details
+        $(".design").click(function () {
+            $("#imageofdesign, #designdetails").toggle();
+        })
+    
+        //development
+        $(".devolop").click(function () {
+            $("#imageofdevelopment, #developmentdetails").toggle();
+        })
+    
+        //product
+        $(".product").click(function () {
+            $("#imageofproducts, #productdetails").toggle();
+        })
+    
+        //adding hover effect
+    
+      //projectpic4
+      $('.project1').hover(function () {
+        $(this).css("opacity", "0.5");
+        $('.p1').removeClass('text');
+    }, function () {
+        $(this).css("opacity", "1");
+        $('.p1').addClass('text');
+    });
+       
+    
+    
+    //projectpic3
+    $('.project2').hover(function () {
+        $(this).css("opacity", "0.5");
+        $('.p2').removeClass('text');
+    }, function () {
+        $(this).css("opacity", "1");
+        $('.p2').addClass('text');
+    });
+     
+    //projectpic2
+    $('.project3').hover(function () {
+        $(this).css("opacity", "0.5");
+        $('.p3').removeClass('text');
+    }, function () {
+        $(this).css("opacity", "1");
+        $('.p3').addClass('text');
+    });
+    
+    //projectpic1
+    
+    $('.project4').hover(function () {
+        $(this).css("opacity", "0.5");
+        $('.p4').removeClass('text');
+    }, function () {
+        $(this).css("opacity", "1");
+        $('.p4').addClass('text');
+    });
+      /// images in the second row
+    
+    //projectpic5
+    
+    $('.project5').hover(function () {
+        $(this).css("opacity", "0.5");
+        $('.p5').removeClass('text');
+    }, function () {
+        $(this).css("opacity", "1");
+        $('.p5').addClass('text');
+    });
+    
+    
+    //projectpic6
+    $('.project6').hover(function () {
+        $(this).css("opacity", "0.5");
+        $('.p6').removeClass('text');
+    }, function () {
+        $(this).css("opacity", "1");
+        $('.p6').addClass('text');
+    });
+    
+    //projectpic7
+    
+    $('.project7').hover(function () {
+        $(this).css("opacity", "0.5");
+        $('.p7').removeClass('text');
+    }, function () {
+        $(this).css("opacity", "1");
+        $('.p7').addClass('text');
+    });
+    
+    //projectpic8
+    
+    $('.project8').hover(function () {
+        $(this).css("opacity", "0.5");
+        $('.p8').removeClass('text');
+    }, function () {
+        $(this).css("opacity", "1");
+        $('.p8').addClass('text');
+    });
+    
+    // form validation
+    document.getElementById('submit').addEventListener('click', function () {
+        var myName = $("#namebar").val();
+        var myMail = $("#emailbar").val();
+        var myMessage = $("#messagebar").val();
+        if ($("#namebar").val() && $("#emailbar").val() && $("#messagebar").val()) {
+            alert("Hi " + myName + ", Thankyou for contacting us.we will get in touch");
+        } else {
+            alert("Please fill all !");
+        }
+    })
+    
+    
+    })

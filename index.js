bd6b9d32b31b04f2e9d03b20c6be5b4ea748745e
Nancy-codergirl <nancymuriithi@student.moringaschool.com>
2021-09-14 16:32:20 +0300

    Add hover effect to portfolio section

--- index.js
+++ index.js
@@ -27,3 +27,73 @@ $(".product").click(function () {
 
 //adding hover effect
 
+$('.project1').hover(function () {
+    $(this).css("opacity", "0.5");
+    $('.p1').removeClass('text');
+}, function () {
+    $(this).css("opacity", "1");
+    $('.p1').addClass('text');
+});
+//projectpic2
+$('.project3').hover(function () {
+    $(this).css("opacity", "0.5");
+    $('.p3').removeClass('text');
+}, function () {
+    $(this).css("opacity", "1");
+    $('.p3').addClass('text');
+});
+
+//projectpic3
+$('.project2').hover(function () {
+    $(this).css("opacity", "0.5");
+    $('.p2').removeClass('text');
+}, function () {
+    $(this).css("opacity", "1");
+    $('.p2').addClass('text');
+});
+ 
+//projectpic6
+$('.project6').hover(function () {
+    $(this).css("opacity", "0.5");
+    $('.p6').removeClass('text');
+}, function () {
+    $(this).css("opacity", "1");
+    $('.p6').addClass('text');
+});
+//projectpic8
+
+$('.project8').hover(function () {
+    $(this).css("opacity", "0.5");
+    $('.p8').removeClass('text');
+}, function () {
+    $(this).css("opacity", "1");
+    $('.p8').addClass('text');
+});
+
+//projectpic7
+
+$('.project7').hover(function () {
+    $(this).css("opacity", "0.5");
+    $('.p7').removeClass('text');
+}, function () {
+    $(this).css("opacity", "1");
+    $('.p7').addClass('text');
+});
+
+//projectpic5
+
+$('.project5').hover(function () {
+    $(this).css("opacity", "0.5");
+    $('.p5').removeClass('text');
+}, function () {
+    $(this).css("opacity", "1");
+    $('.p5').addClass('text');
+});
+$('.project4').hover(function () {
+    $(this).css("opacity", "0.5");
+    $('.p4').removeClass('text');
+}, function () {
+    $(this).css("opacity", "1");
+    $('.p4').addClass('text');
+});
+
